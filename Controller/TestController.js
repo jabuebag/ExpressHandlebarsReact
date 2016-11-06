0c9efa7709b8d513848237c14b569ab7a9b46714
jabuebag <ybagyang@gmail.com>
2016-11-05 22:34:02 -0700

    add map rout

--- Controller/TestController.js
+++ Controller/TestController.js
@@ -13,15 +13,36 @@ router.get('/test', function (req, res, next) {
     res.render('index', {
         layout: 'main',
         test: ReactDOMServer.renderToString(element),
-        people: ['chris','lucia'],
+        people: ['chris', 'lucia'],
         parttest: 'houhou',
         title: 'titletest'
     });
 });
 
+router.get('/addRoute', function (req, res, next) {
+    res.render('addRoute', {
+        layout: 'main',
+        title: 'Map Add Route'
+    });
+});
+
+router.get('/routes', function (req, res, next) {
+    res.render('routes', {
+        layout: 'main',
+        title: 'Map Routes'
+    });
+});
+
+router.get('/mapTest', function (req, res, next) {
+    res.render('mapTest', {
+        layout: 'main',
+        title: 'Map Test'
+    });
+});
+
 // send static files, for testing
-router.get('/test1', function(req, res, next) {
-    res.sendFile( process.cwd() + '/View/separate.html');
+router.get('/test1', function (req, res, next) {
+    res.sendFile(process.cwd() + '/View/separate.html');
 });
 
 module.exports = router;
